9587a56ae4a39b041470f2d4768d169bee660c17
Kris <krzysztof.szewczyk.ksz@gmail.com>
2019-03-24 22:47:53 +0100

    Make separate 'dev' | 'prod' apps.

--- web/index_dev.php
+++ web/index_dev.php
@@ -5,6 +5,7 @@ use SimpleExampleApp\Application\PricesController;
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new DI\Bridge\Silex\Application();
+$app['debug'] = true;
 
 $app->get('/items/{itemId}/price/{countryCode}/', [PricesController::class, 'getItemPrice']);
 
